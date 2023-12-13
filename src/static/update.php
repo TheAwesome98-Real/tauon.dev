0b3bb49884a8308b5b94a1cf9f3787918922df7f
TheAwesome98 <TheAwesome98@programmer.net>
2023-12-13 15:24:40 +0000

    no it failed

--- src/static/update.php
+++ src/static/update.php
@@ -3,6 +3,7 @@ chdir("..");
 $provided_secret = $_SERVER['HTTP_X_HUB_SIGNATURE_256'];
 $real_secret = explode("=", file_get_contents('.env'));
 if (!hash_equals("sha256=" . hash_hmac("sha256", file_get_contents('php://input'), $real_secret[1], true), $provided_secret)) {
+  http_response_code(400);
   die("you are not github");
 } else {
   system(escapeshellcmd("yarn update"));
