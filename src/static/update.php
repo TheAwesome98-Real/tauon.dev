ab72fca967ce1010ee36437f0865a0623ba61f0f
TheAwesome98 <TheAwesome98@programmer.net>
2023-12-13 15:19:30 +0000

    this cat can't read documentation

--- src/static/update.php
+++ src/static/update.php
@@ -1,9 +1,10 @@
 <?php
 chdir("..");
-$provided_secret = $_SERVER['HTTP_X_GITHUB_EVENT'];
-$real_secret = explode("=", fread(fopen(".env", "r"),filesize($url)));
-if $provided_secret != $real_secret[1] {
+$provided_secret = $_SERVER['HTTP_X_HUB_SIGNATURE_256'];
+$real_secret = explode("=", file_get_contents('.env'));
+if (!hash_equals("sha256=" . hash_hmac("sha256", file_get_contents('php://input'), $real_secret[1], true), $provided_secret)) {
   die("you are not github");
+} else {
+  system(escapeshellcmd("yarn update"));
 }
-system(escapeshellcommand("yarn update"));
 ?>
