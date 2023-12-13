0bb353aca2763d5789d0b75e359257a1eb36f551
TheAwesome98 <TheAwesome98@programmer.net>
2023-12-13 15:31:54 +0000

    cat can't read part 9 million

--- src/static/update.php
+++ src/static/update.php
@@ -2,7 +2,7 @@
 chdir("..");
 $provided_secret = $_SERVER['HTTP_X_HUB_SIGNATURE_256'];
 $real_secret = explode("=", file_get_contents('.env'));
-$hash = "sha256=" . hash_hmac("sha256", file_get_contents('php://input'), $real_secret[1], true);
+$hash = "sha256=" . hash_hmac("sha256", file_get_contents('php://input'), $real_secret[1]);
 var_dump($hash, $provided_secret);
 if (!hash_equals($hash, $provided_secret)) {
   http_response_code(400);
