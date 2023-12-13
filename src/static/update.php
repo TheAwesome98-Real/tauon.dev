de9cab46a5f7b1f0b36e0e2f18192f21bdefb259
TheAwesome98 <TheAwesome98@programmer.net>
2023-12-13 16:29:13 +0000

    su

--- src/static/update.php
+++ src/static/update.php
@@ -1,13 +1,14 @@
 <?php
 chdir("..");
 $provided_secret = $_SERVER['HTTP_X_HUB_SIGNATURE_256'];
-$real_secret = explode("=", file_get_contents('.env'));
+$config = parse_ini_file(".env");
+$real_secret = explode("=", $config["secret"]);
 $hash = "sha256=" . hash_hmac("sha256", file_get_contents('php://input'), $real_secret[1]);
 if (!hash_equals($hash, $provided_secret)) {
   http_response_code(400);
   die("you are not github");
 } else {
   echo("success it worked");
-  system(escapeshellcmd("yarn update"));
+  system(escapeshellcmd("su -c " . $config["user"] . " ./update.sh"));
 }
 ?>
