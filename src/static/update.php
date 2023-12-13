505541afa1603e26a1b98d66900073446d71e4c6
TheAwesome98 <TheAwesome98@programmer.net>
2023-12-13 16:08:25 +0000

    add confirmation

--- src/static/update.php
+++ src/static/update.php
@@ -7,6 +7,7 @@ if (!hash_equals($hash, $provided_secret)) {
   http_response_code(400);
   die("you are not github");
 } else {
+  echo("success it worked");
   system(escapeshellcmd("yarn update"));
 }
 ?>
