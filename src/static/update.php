27fc0500353645d1850503d9d8cc32d11babde06
TheAwesome98 <TheAwesome98@programmer.net>
2023-12-13 15:30:16 +0000

    test more

--- src/static/update.php
+++ src/static/update.php
@@ -2,8 +2,9 @@
 chdir("..");
 $provided_secret = $_SERVER['HTTP_X_HUB_SIGNATURE_256'];
 $real_secret = explode("=", file_get_contents('.env'));
-var_dump($real_secret, $provided_secret);
-if (!hash_equals("sha256=" . hash_hmac("sha256", file_get_contents('php://input'), $real_secret[1], true), $provided_secret)) {
+$hash = "sha256=" . hash_hmac("sha256", file_get_contents('php://input'), $real_secret[1], true);
+var_dump($hash, $provided_secret);
+if (!hash_equals($hash, $provided_secret)) {
   http_response_code(400);
   die("you are not github");
 } else {
