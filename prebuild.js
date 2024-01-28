853ea7a4ec1255b59b90384bfb6e96f17632978d
TheAwesome98 <TheAwesome98@programmer.net>
2024-01-28 17:44:55 +0000

    stop overthinking things

---
+++ prebuild.js
@@ -0,0 +1,38 @@
+const http = require("http");
+const https = require("https");
+const filesystem = require("fs");
+const eightyeightbythirtyones = JSON.parse(
+  filesystem.readFileSync("src/_data/88x31s.json")
+);
+
+filesystem.renameSync("src/static/images/88x31/3rdparty", "3rdparty.bak");
+
+filesystem.mkdirSync("src/static/images/88x31/3rdparty/");
+
+for (let set of eightyeightbythirtyones) {
+  for (let button of set.entries) {
+    let client = null;
+    if (button.src.startsWith("https")) {
+      client = https;
+    } else if (button.src.startsWith("http")) {
+      client = http;
+    }
+    if (client != null) {
+      client.get(button.src, (res) => {
+        let file = filesystem.createWriteStream(
+          "src/static/images/88x31/3rdparty/" +
+            button.src.replaceAll("/", "_").replaceAll(":", "_")
+        );
+        res.pipe(file);
+
+        file.on("finish", () => {
+          file.close();
+        });
+
+        res.on("error", (err) => {
+          console.error("failed to download", button.src, ":", err);
+        });
+      });
+    }
+  }
+}
